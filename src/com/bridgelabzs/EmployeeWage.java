e26d55f424c517977adde46ffc7693dfe7edca6f
sheetal-10 <sheetalp257@gmail.com>
2021-08-05 13:23:34 +0530

    Added: code for part time employee wage

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -9,21 +9,29 @@ public class EmployeeWage {
 	
 	/** 
 	 * this is the main method that
-	 * checks for employee attendane
+	 * checks for employee attendane and
+	 * calculate wage
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		//constatnt
 		int IS_FULLTIME_PRESENT = 1;
+		int IS_PART_TIME = 2;
 		int WAGE_PER_HR = 20;
 		//variables
 		int daily_wage;
 		int working_hr;
+		//computation
 		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
 		if( emp_check == IS_FULLTIME_PRESENT)
 		{
-			System.out.println("Employee is present");
+			System.out.println("Employee is full time present");
 			working_hr = 8;
+		}
+		else if( emp_check == IS_PART_TIME)
+		{
+			System.out.println("Employee is part time present");
+			working_hr = 4;
 		}
 		else
 		{
