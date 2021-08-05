a001404fd3f8ad77eb6281a660a6987c2acc16a1
sheetal-10 <sheetalp257@gmail.com>
2021-08-05 13:31:29 +0530

    Refactored : code for daily wage

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -9,16 +9,18 @@ public class EmployeeWage {
 	
 	/** 
 	 * this is the main method that
-	 * checks for employee attendane
+	 * checks for employee attendane and
+	 * calculate wage
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		//constatnt
+		//constatnts
 		int IS_FULLTIME_PRESENT = 1;
 		int WAGE_PER_HR = 20;
 		//variables
 		int daily_wage;
 		int working_hr;
+		//computation
 		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
 		if( emp_check == IS_FULLTIME_PRESENT)
 		{
