76ef6f3195fdae4ac72fdd705861d1d3990fff82
sheetal-10 <sheetalp257@gmail.com>
2021-08-05 10:03:31 +0530

    Added: code for daily employee wage

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -10,19 +10,27 @@ public class EmployeeWage {
 	/** 
 	 * this is the main method that
 	 * checks for employee attendane
-	 * @param args unused
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		//constatnt
-		int IS_PRESENT = 1;
-		//computation
+		int IS_FULLTIME_PRESENT = 1;
+		int WAGE_PER_HR = 20;
+		//variables
+		int daily_wage;
+		int working_hr;
 		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
-		if( emp_check == IS_PRESENT)
+		if( emp_check == IS_FULLTIME_PRESENT)
 		{
 			System.out.println("Employee is present");
+			working_hr = 8;
 		}
 		else
+		{
 			System.out.println("Employee is absent");
+			working_hr = 0;
+	        }  
+		daily_wage = working_hr * WAGE_PER_HR;
+		System.out.println("Employee daily wage is " + daily_wage);
 	}
 }
