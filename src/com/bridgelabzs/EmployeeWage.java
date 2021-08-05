d1b7ce2b904978cd270007ce6ddd97c627628b73
sheetal-10 <sheetalp257@gmail.com>
2021-08-05 20:10:46 +0530

    Refactored: employee wage using switch case

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -16,22 +16,26 @@ public class EmployeeWage {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		//constatnts
 		int IS_FULLTIME_PRESENT = 1;
+		int IS_PARTTIME_PRESENT = 2;
 		int WAGE_PER_HR = 20;
 		//variables
 		int daily_wage;
 		int working_hr;
 		//computation
 		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
-		if( emp_check == IS_FULLTIME_PRESENT)
-		{
-			System.out.println("Employee is present");
+		switch (emp_check) {
+		case 1:
+			System.out.println("Employee is full time present");
 			working_hr = 8;
-		}
-		else
-		{
+			break;
+		case 2:
+			System.out.println("Employee is part time present");
+			working_hr = 4;
+			break;
+		default:
 			System.out.println("Employee is absent");
 			working_hr = 0;
-	        }  
+	}
 		daily_wage = working_hr * WAGE_PER_HR;
 		System.out.println("Employee daily wage is " + daily_wage);
 	}
