3e16128cf340e22f22b7ff09081a5715719af29a
sheetal-10 <sheetalp257@gmail.com>
2021-08-05 20:21:00 +0530

    Added: code for caculating monthly wage

--- src/com/bridgelabzs/EmployeeWage.java
+++ src/com/bridgelabzs/EmployeeWage.java
@@ -10,7 +10,7 @@ public class EmployeeWage {
 	/** 
 	 * this is the main method that
 	 * checks for employee attendane and
-	 * calculate wage
+	 * calculate wages for a month
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
@@ -20,23 +20,30 @@ public class EmployeeWage {
 		int WAGE_PER_HR = 20;
 		//variables
 		int daily_wage;
-		int working_hr;
+		int working_hr = 0;
+		int monthly_total_wage = 0;
+		int days_in_month=20;
+		int daily_wage_array[] = new int[20];
 		//computation
+		for (int i=0; i<days_in_month; i++) {
 		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
 		switch (emp_check) {
 		case 1:
-			System.out.println("Employee is full time present");
 			working_hr = 8;
 			break;
 		case 2:
-			System.out.println("Employee is part time present");
 			working_hr = 4;
 			break;
 		default:
-			System.out.println("Employee is absent");
 			working_hr = 0;
+		}
+		daily_wage_array[i] = working_hr * WAGE_PER_HR;
 	}
-		daily_wage = working_hr * WAGE_PER_HR;
-		System.out.println("Employee daily wage is " + daily_wage);
+		for (int j=0; j<days_in_month; j++) {
+			int day = j+1;
+			System.out.println("Day " + day + " wage is " + daily_wage_array[j]);
+			monthly_total_wage = monthly_total_wage + daily_wage_array[j];
+		}
+		System.out.println("\nMonthly wage is " + monthly_total_wage);
 	}
 }
